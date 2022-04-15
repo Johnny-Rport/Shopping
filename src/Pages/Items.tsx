9bfe2fa1f2ef15ec8b40df19b2108d72d9e5566e
Johnny Rangel <jrangel18419@gmail.com>
2022-04-15 02:58:51 -0700

    Updating Font

--- src/Pages/Items.tsx
+++ src/Pages/Items.tsx
@@ -15,27 +15,27 @@ function Items() {
     <div className={style.grid}>
       <div>
           <img src={vaccumPic} alt='looks like its broken'></img> <button className={style.buyBut}>Buy Now!</button>
-          <p className='content'>This Premium Vaccum gets cleaning done like never before! It has amazing speed and will make your day easy.</p>
+          <p className={style.content}>This Premium Vaccum gets cleaning done like never before! It has amazing speed and will make your day easy.</p>
         </div>
         <div>
           <img src={broomPic} alt='looks like its broken'></img> <button className={style.buyBut}>Buy Now!</button>
-          <p className='content'>The Premium Broom is one of our toughest appliances and will get the job done in a way you've never seen!</p>
+          <p className={style.content}>The Premium Broom is one of our toughest appliances and will get the job done in a way you've never seen!</p>
         </div>
         <div>
           <img src={dishwasherPic} alt='looks like its broken'></img> <button className={style.buyBut}>Buy Now!</button>
-          <p className='content'>A Premium Dishwasher for the best and most premium plates. This dishwasher will make your food taste better!</p>
+          <p className={style.content}>A Premium Dishwasher for the best and most premium plates. This dishwasher will make your food taste better!</p>
         </div>
         <div>
           <img src={laundryPic} alt='looks like its broken'></img> <button className={style.buyBut}>Buy Now!</button>
-          <p className='content'>Have a huge load to clean, wash, and dry? Do not worry for the Premium Washer and Dryer combo will fix all your problems!</p>
+          <p className={style.content}>Have a huge load to clean, wash, and dry? Do not worry for the Premium Washer and Dryer combo will fix all your problems!</p>
         </div>
         <div>
           <img src={lawnPic} alt='looks like its broken'></img> <button className={style.buyBut}>Buy Now!</button>
-          <p className='content'>This Premium Lawnmower will cut forests down like nothing. So make sure to use its power well in accordance to your lawn.</p>
+          <p className={style.content}>This Premium Lawnmower will cut forests down like nothing. So make sure to use its power well in accordance to your lawn.</p>
         </div>
         <div>
           <img src={hosePic} alt='looks like its broken'></img> <button className={style.buyBut}>Buy Now!</button>
-          <p className='content'>Used by our proud firefighters, the Premium Hose has enough hosepower to put out a many fires. Get yours NOW!</p>
+          <p className={style.content}>Used by our proud firefighters, the Premium Hose has enough hosepower to put out a many fires. Get yours NOW!</p>
         </div>
     </div>
 
