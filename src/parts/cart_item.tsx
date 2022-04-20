a1966003a007ca0ca3e70752285f6dd799a9bc07
Johnny Rangel <jrangel18419@gmail.com>
2022-04-20 03:59:43 -0700

    Updating Cart

--- src/parts/cart_item.tsx
+++ src/parts/cart_item.tsx
@@ -1,8 +1,18 @@
 import React from 'react';
+import style from '../Pages/css/home.module.css'
 
 function Cartitem(){
     return (
-        <div>Item breakdown here, list all items in cart, allow an option to see details, maybe description?</div>
+        <div className={style.list}> <h4>Items in Cart (Mock)</h4>
+            <ul>
+                <li>Apples <button>Details</button></li>
+                <li>Soda  <button>Details</button></li>
+                <li>Broom  <button>Details</button></li>
+                <li>Silverware  <button>Details</button></li>
+            </ul>
+
+            <div>Template above: Lists items (without quantity) followed by button that will give item description in the item page in a format like this</div>
+        </div>
     )
 }
 
